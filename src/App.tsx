105d33fe5703ca6fb826d8b173a642c13a0b1ce9
Alex Silva <9apsilva@gmail.com>
2022-12-25 21:08:35 -0300

    create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
+import { GlobalStyle } from "./GlobalStyles"
 
 export const App = () => {
   return (
     <>
       <Router>
+        <GlobalStyle />
         <Navbar />
       </Router>
     </>
