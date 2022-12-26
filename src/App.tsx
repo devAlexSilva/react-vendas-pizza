dd943e53afa190de662fbb72e0a67fc57a228b10
Alex Silva <9apsilva@gmail.com>
2022-12-25 21:11:59 -0300

    refactor: change components path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import { Navbar } from "./components/Navbar"
+import { Navbar } from "./components/navbar"
 import { GlobalStyle } from "./GlobalStyles"
 
 export const App = () => {
